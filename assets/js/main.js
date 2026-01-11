0753a769df09ea0b696e4cbc72a18e674f9e9308
Mhafis99 <mhafizhsetiawan82@gmail.com>
2026-01-11 13:50:11 +0700

    fix html js path and safe js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,15 @@ function comingSoon() {
 }
 
 const searchInput = document.getElementById("searchInput");
-const cards = document.querySelectorAll(".card");
 
-searchInput.addEventListener("keyup", function () {
-  const keyword = searchInput.value.toLowerCase();
+if (searchInput) {
+  const cards = document.querySelectorAll(".card");
 
-  cards.forEach(card => {
-    const name = card.getAttribute("data-name");
-    card.style.display = name.includes(keyword) ? "block" : "none";
+  searchInput.addEventListener("keyup", function () {
+    const keyword = searchInput.value.toLowerCase();
+    cards.forEach(card => {
+      const name = card.getAttribute("data-name");
+      card.style.display = name.includes(keyword) ? "block" : "none";
+    });
   });
-});
+}
